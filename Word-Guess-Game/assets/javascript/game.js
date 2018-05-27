5dec2765c729788c217d1f13e6f72113b1bcb738
Vger <vcdelariva@gmail.com>
2018-05-27 14:10:27 -0500

    Move css and js into appropriate files

---
+++ Word-Guess-Game/assets/javascript/game.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+
+    var possibleWords = ["florence", "paris", "madrid", "rome", "singapore", "dubai", "new york city", 
+                            "shanghai", "london", "tokyo", "sydney", "toronto", "beijing", "moscow",
+                            "johannesburg", "istanbul", "warsaw", "jakarta", "kuala lumpur", "mexico city",
+                            "hong kong", "chicago", "seoul", "los angeles", "mumbai"]
+
+    const maxGuess = 10
+
+    var guessedLetters = []
+    var guessingWord = []
+    var wordToMatch
+    var numGuess
+    var wins = 0
+
+    resetGame()
+
+    // Wait for key press
+    document.onkeypress = function(event) {
+        // Make sure key pressed is an alpha character
+        if (isAlpha(event.key)) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+
+    // Game Functions
+    function checkForLetter(letter) {
+        var foundLetter = false
+        var correctSound = document.createElement("audio")
+        var incorrectSound = document.createElement("audio")
+        correctSound.setAttribute("src", "assets/sounds/stairs.mp3")
+        incorrectSound.setAttribute("src","assets/sounds/croak.mp3")
+
+        // Search string for letter
+        for (var i=0, j= wordToMatch.length; i<j; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                correctSound.play()
+            }
+        }
+
+        if (!foundLetter) {
+            incorrectSound.play()
+            // Check if inccorrect guess is already on the list
+            if (!guessedLetters.includes(letter)) {
+                // Add incorrect letter to guessed letter list
+                guessedLetters.push(letter)
+                // Decrement the number of remaining guesses
+                numGuess--
+            }
+            if (numGuess === 0) {
+                resetGame()
+            }
+        }
+        // If guessing word matches random word
+        if (guessingWord.join("") === wordToMatch) {
+            // Increment # of wins
+            wins++
+            resetGame()
+        }
+
+        updateDisplay()
+
+    }
+    // Check in keypressed is between A-Z or a-z
+    function isAlpha (ch){
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        numGuess = maxGuess
+
+        // Get a new word
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
+
+        // Reset word arrays
+        guessedLetters = []
+        guessingWord = []
+
+        // Reset the guessed word
+        for (var i=0, j=wordToMatch.length; i < j; i++){
+            // Put a space instead of an underscore between multi word "words"
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
+            }
+        }
+
+        // Update the Display
+        updateDisplay()
+    }
+
+    function updateDisplay () {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentWord").innerText = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ")
+    }
+})
